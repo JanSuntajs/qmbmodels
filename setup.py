72a7849f249681e780beedeeaae89141a1dad837
Jan Suntajs <jansuntajs@gmail.com>
2019-11-04 15:18:04 +0100

    Added a basic installation script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='qmbmodels',
+      version='1.0.0',
+      description='A module for calculations with 1D quantum hamiltonians',
+      url='https://github.com/JanSuntajs/qmbmodels',
+      author='Jan Suntajs',
+      author_email='Jan.Suntajs@ijs.si',
+      license='MIT',
+      packages=[],
+      install_requires=['ham1d'],
+      dependency_links=[('https://github.com/JanSuntajs/'
+                         'ham1d/tarball/'
+                         'master/#egg=ham1d-1.0.0')],
+      zip_safe=False)
