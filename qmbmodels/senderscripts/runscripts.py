fa4fa07e0126daa23c91f87523e3d2959277d873
Jan Suntajs <jansuntajs@gmail.com>
2019-11-08 20:48:10 +0100

    removing enumerate in runscripts.prepare_dep...

--- qmbmodels/senderscripts/runscripts.py
+++ qmbmodels/senderscripts/runscripts.py
@@ -342,7 +342,7 @@ def prepare_dependency_script(scripts, name, *args, **kwargs):
         # remove the scripts to avoid cluttering
         # of the folder
 
-        for script in enumerate(scripts):
+        for script in scripts:
 
             # copy_path to a dedicated subfolder in the
             # results folder and remove the folder from
