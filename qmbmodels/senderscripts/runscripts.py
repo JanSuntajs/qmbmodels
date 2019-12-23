5fc2b9794094e8dbe4148bb312ccdda62a71a8cf
Jan Suntajs <jansuntajs@gmail.com>
2019-12-23 14:11:25 +0100

    still fixing

--- qmbmodels/senderscripts/runscripts.py
+++ qmbmodels/senderscripts/runscripts.py
@@ -60,7 +60,7 @@ programs = {
 
     'diag': {'name': './main_diag.py', 'array': True,
              'save': 'Eigvals', 'vectors': True,
-             'noqueue': False, 'mpi': True,
+             'noqueue': False, 'mpi': False,
              'short': True, 'postprocess': False},
     'sff': {'name': './main_sff.py', 'array': False,
             'save': 'Spectral_stats', 'vectors': False,
