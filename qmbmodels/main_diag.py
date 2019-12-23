cc79c9cf0e146c66ae29ecd66addb0a2528ad034
Jan Suntajs <jansuntajs@gmail.com>
2019-12-23 12:20:58 +0100

    making sure diagonalization code can also work for shorter jobs

--- qmbmodels/main_diag.py
+++ qmbmodels/main_diag.py
@@ -12,25 +12,33 @@ save_metadata = True
 if __name__ == '__main__':
 
     (mod, model_name, argsDict, seedDict, syspar_keys,
-     modpar_keys, savepath, syspar, modpar, *rest) = get_module_info()
+     modpar_keys, savepath, syspar, modpar, min_seed,
+     max_seed) = get_module_info()
 
-    print('Using seed: {}'.format(argsDict['seed']))
+    save_metadata = True
 
-    # get the instance of the appropriate hamiltonian
-    # class and the diagonal random fields used
-    model, fields = mod.construct_hamiltonian(argsDict)
+    for seed in range(min_seed, max_seed + 1):
+        print('Using seed: {}'.format(seed))
+        argsDict['seed'] = seed
+        # get the instance of the appropriate hamiltonian
+        # class and the diagonal random fields used
+        model, fields = mod.construct_hamiltonian(argsDict)
 
-    print('Starting diagonalization ...')
-    eigvals = model.eigvals(complex=False)
-    # eigvals, eigvecs = model.eigsystem(complex=False, turbo=True)
-    print('Diagonalization finished!')
+        print('Starting diagonalization ...')
+        eigvals = model.eigvals(complex=False)
+        # eigvals, eigvecs = model.eigsystem(complex=False, turbo=True)
+        print('Diagonalization finished!')
 
-    print('Displaying eigvals')
-    print(eigvals)
+        print('Displaying eigvals')
+        print(eigvals)
 
-    # ----------------------------------------------------------------------
-    # save the files
-    eigvals_dict = {'Eigenvalues': eigvals,
-                    **fields}
-    savefile(eigvals_dict, savepath, syspar, modpar, argsDict,
-             syspar_keys, modpar_keys, 'full', save_metadata, save_type='npz')
+        # ----------------------------------------------------------------------
+        # save the files
+        eigvals_dict = {'Eigenvalues': eigvals,
+                        **fields}
+
+        savefile(eigvals_dict, savepath, syspar, modpar, argsDict,
+                 syspar_keys, modpar_keys, 'full',
+                 save_metadata, save_type='npz')
+
+        save_metadata = False
