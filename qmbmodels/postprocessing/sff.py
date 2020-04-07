b051186ef6dde2963ac535f08cbaf358b861d87a
Jan Suntajs <jansuntajs@gmail.com>
2020-04-07 18:15:19 +0200

    change rw permissions

--- qmbmodels/postprocessing/sff.py
+++ qmbmodels/postprocessing/sff.py
@@ -407,7 +407,7 @@ def get_sff(h5file, results_key='SFF_spectrum',
         eta, sff_filter)
 
     try:
-        with h5py.File(h5file, 'r') as file:
+        with h5py.File(h5file, 'r+') as file:
 
             # if 'eta_eta' in results_key:
             results_key_ = results_key.replace('eta_eta', 'eta')
