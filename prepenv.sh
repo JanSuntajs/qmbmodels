962682e0023c4b27d966f646dcd4631d8ad490e4
Jan Suntajs <jansuntajs@gmail.com>
2021-02-15 19:54:10 +0100

    fetching conda_spec_file.txt

--- prepenv.sh
+++ prepenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 conda deactivate
-conda create --name petscenv --file conda_spec_file.txt
-conda activate petscenv
+conda create --name petscenv3 --file conda_spec_file.txt
+conda activate petscenv3
 
 pip install . --process-dependency-links
