fixing the definition in the matelts variance for 2D anderson

qmbmodels/mainscripts/main_matelts_variance_ratio_partial.py
@@ -101,7 +101,8 @@ def _get_variance_ratio(row1, row2, hop_operator=False):
     offdiag = operator[mask_]
 
     var_diag = (np.mean(diag**2) - np.mean(diag)**2)
-    var_offdiag = np.mean(np.abs(offdiag)**2)
+    # var_offdiag = np.mean(np.abs(offdiag)**2)
+    var_offdiag = np.std(offdiag)**2
     var_ratio = var_diag / var_offdiag
 
     return np.real(var_ratio)